af26a2d38082a8cb98e9276e7822405fe91ce05b
Josue Henrique <josue21henrique@gmail.com>
2025-05-30 14:54:50 -0300

    teste de pastas

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,12 +1,18 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 import { Request } from 'express';
 
 export const createMulter = (folder: string) => {
   const storage = multer.diskStorage({
     destination: (_req: Request, _file: Express.Multer.File, cb) => {
       const dir = path.join('uploads', folder);
-      cb(null, dir); 
+      
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+      }
+      
+      cb(null, dir);
     },
     filename: (_req: Request, file: Express.Multer.File, cb) => {
       cb(null, Date.now() + path.extname(file.originalname));
