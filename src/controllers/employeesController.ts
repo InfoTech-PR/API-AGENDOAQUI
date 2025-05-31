712ee48662c3b82547a558e90ed8a1f4d01aa405
Josue Henrique <josue21henrique@gmail.com>
2025-05-31 10:45:43 -0300

    testeando employees

--- src/controllers/employeesController.ts
+++ src/controllers/employeesController.ts
@@ -6,7 +6,7 @@ export const registerEmployees = async (req: Request, res: Response) => {
   try {
     const { id_business, name, summary, specialization } = req.body;
     const image = req.file ? `/uploads/employees/${req.file.filename}` : null;
-
+    console.log('req.file:', req.file);
     const idBusinessNum = Number(id_business);
 
     if (!idBusinessNum || !name || !specialization || isNaN(idBusinessNum)) return res.status(400).json({ message: 'Campos obrigatórios inválidos ou vazios!' });
